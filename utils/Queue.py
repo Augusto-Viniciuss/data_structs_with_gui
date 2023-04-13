c0cfea3030e79b9847d2b18f0ab80e801067b055
augustoviniciud@gmail.com <augustoviniciud@gmail.com>
2023-04-12 21:24:59 -0300

    Done stack and queue structures

---
+++ utils/Queue.py
@@ -0,0 +1,44 @@
+from .Node import Node
+
+class Queue:
+    def __init__(self):
+        self.begin = None
+        self.end = None
+        self.qtd_elements = 0
+        
+    def is_empty(self):
+        if self.qtd_elements == 0:
+            return True
+        else:
+            return False
+    
+    def get_size(self):
+        return self.qtd_elements
+
+    def get_begin_element(self):
+        if not self.is_empty():
+            return self.begin.get_value()
+        else:
+            return None
+    
+    def insert(self, value):
+        if self.is_empty():
+            aux_node = Node(value)
+            self.begin = self.end = aux_node
+        else:
+            aux_node = Node(value)
+            self.end.set_next_node(aux_node)
+            self.end = aux_node
+        
+        self.qtd_elements += 1
+            
+        return True
+            
+    def remove(self):
+        if not self.is_empty():
+            self.begin = self.begin.get_next_node()
+            self.qtd_elements -= 1
+            
+            return True
+        else:
+            return False
---
+++ utils/Stack.py
@@ -0,0 +1,41 @@
+from .Node import Node
+
+class Stack:
+    def __init__(self, ):
+        self.top = None
+        self.qtd_elements = 0
+        
+    def is_empty(self):
+        if self.qtd_elements == 0:
+            return True
+        else:
+            return False
+                
+    def get_size(self):
+        return self.qtd_elements
+        
+    def get_top(self):
+        if not self.is_empty():
+            return self.top.get_value()
+        else:
+            return None
+        
+    def push(self, value):
+        if self.is_empty():
+            self.top = Node(value)
+        else:
+            aux_node = Node(value, self.top)
+            self.top = aux_node
+            
+        self.qtd_elements += 1
+        
+        return True
+            
+    def pop(self):
+        if self.is_empty():
+            return False
+        else:
+            self.top = self.top.get_next_node()
+            self.qtd_elements -= 1
+            
+            return True
