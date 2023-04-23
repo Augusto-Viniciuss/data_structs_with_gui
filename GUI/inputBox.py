5ba98e670a057091994cee7258b3e0fe106462b2
G4burieru <joao.gabriel.silva.santos@academico.ufpb.br>
2023-04-22 22:16:55 -0300

    finally input box is working yaaay

---
+++ GUI/inputBox.py
@@ -0,0 +1,41 @@
+import pygame
+
+class InputBox:
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = pygame.Color('lightskyblue3')
+        self.text = text
+        self.txt_surface = pygame.font.Font(None, 32).render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the color of the input box.
+            self.color = pygame.Color('dodgerblue2') if self.active else pygame.Color('lightskyblue3')
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    # If the user pressed Return, clear the input box and print the input.
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    # If the user pressed Backspace, remove the last character from the input.
+                    self.text = self.text[:-1]
+                elif event.key == pygame.K_ESCAPE:
+                    self.active = False
+                else:
+                    # If the user typed a character, add it to the input.
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = pygame.font.Font(None, 32).render(self.text, True, self.color)
+
+    def draw(self, screen):
+        # Blit the input box rect and text surface onto the screen.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
--- GUI/sequentialList.py
+++ GUI/sequentialList.py
@@ -1,5 +1,6 @@
 import pygame
 from windowManager import WindowManager
+from inputBox import InputBox
 
 
 
@@ -10,36 +11,32 @@ class SequentialList():
         self.mid_w, self.mid_h = self.wm.DISPLAY_W / 2, self.wm.DISPLAY_H / 2
         self.wm.blit_screen()
         self.flag_input = None
+        self.input_box = InputBox(100, 100, 140, 32)
+        self.input_box2 = InputBox(100, 300, 140, 32)
         
     def show_display(self):
-        
-        inputbox_fake = self.wm.draw_inputbox(self.mid_w, self.mid_h, self.wm.GRAY) #getting the dimensions of the first input box
-        self.wm.blit_screen()       #updating the screen
-        
-        return_function = self.wm.logic_inputbox(inputbox_fake)
-        
-        if return_function == 1 or return_function == -1:
-            return -1     
-    
-        
-    # def show_display(self):
-        
-    #     color_inactive = pygame.Color('gray15')
-    #     self.wm.display.fill(self.wm.BLACK)
-    #     self.wm.draw_text("LISTA SEQUENCIAL", 14, self.mid_w, self.mid_h/2)
-    #     self.wm.blit_screen()
-        
-    #     while True:
-    #         self.input_rect = pygame.Rect(200, 200, 140, 32)
-    #         pygame.draw.rect(self.wm.window, color_inactive, self.input_rect, 2)    #draw the retangle of the text box
+        running = True
+        while running: 
             
-    #         self.wm.display.blit(self.wm.display, self.input_rect)
-            
-    #         self.wm.check_events()
-    #         self.flag_input = self.check_input()
-    #         if self.flag_input != None:
-    #             return self.flag_input
+            self.flag_input = self.check_input()
+            if self.flag_input != None: 
+                return self.flag_input
+
+            self.wm.window.fill(self.wm.WHITE)
+            self.input_box.draw(self.wm.window)
+            self.input_box2.draw(self.wm.window)
+            pygame.display.flip()   
         
-    #         self.wm.blit_screen()
+    def check_input(self):
+        
+        for event in pygame.event.get():    
             
-    # 
+            if event.type == pygame.QUIT:
+                return -1
+            
+            elif event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_ESCAPE:
+                    return 1
+                    
+            self.input_box.handle_event(event)
+            self.input_box2.handle_event(event)
