8f08a2a94421488a5ec51ef8ac90819a423ce061
JulianaDantass <julianadantas498@gmail.com>
2023-04-24 02:44:05 -0300

    design partially fized

--- GUI/sequentialList.py
+++ GUI/sequentialList.py
@@ -9,17 +9,17 @@ class SequentialList():
     def __init__(self):
         self.wm = WindowManager()
         self.list = Sequential_list()
-        self.box_x, self.box_y = 150, 420      #need to define as parameter the x and y of the firstbox
-        self.spacing = 50
+        self.box_x, self.box_y = 170, 420      #need to define as parameter the x and y of the firstbox
+        self.spacing = 75
         self.id = "Sequential list"
         self.mid_w, self.mid_h = self.wm.DISPLAY_W / 2, self.wm.DISPLAY_H / 2
         self.wm.blit_screen()
         self.flag_input = None
-        self.input_box1 = InputBox(self.box_x, self.box_y+75)
-        self.input_box2= InputBox(self.box_x, self.box_y)
-        self.input_box3 = InputBox(self.box_x*3, self.box_y)
-        self.input_box4 = InputBox(self.box_x*4, self.box_y)
-        self.input_box5 = InputBox(self.box_x*4, self.box_y+75)
+        self.input_box1 = InputBox(self.box_x, self.box_y+50)       #box inserir-elemento
+        self.input_box2= InputBox(self.box_x, self.box_y)          
+        self.input_box3 = InputBox(self.box_x*2+30, self.box_y)     
+        self.input_box4 = InputBox(self.box_x*3+60, self.box_y)
+        self.input_box5 = InputBox(self.box_x*3+60, self.box_y+50)
         self.error_add, self.error_remove, self.error_search = False, False, False
         self.square_w,self.square_h = -20, 200
         self.square_side, self.square_border = 70, 2
@@ -35,24 +35,26 @@ class SequentialList():
 
             self.wm.display.fill(self.wm.BLACK)
             
-            self.wm.draw_text("INSERÇÃO", 15, self.box_x+55, self.box_y-30, self.wm.PURPLE) #in the top of first column
-            self.wm.draw_text("REMOÇÃO", 15, self.box_x*3+55, self.box_y-30, self.wm.PURPLE) #in the top of second column
-            self.wm.draw_text("BUSCA", 15, self.box_x*4+55, self.box_y-30, self.wm.PURPLE) #in the top of third column
-            self.wm.draw_text("Posição", 15, self.box_x-self.spacing, self.box_y+15, self.wm.PURPLE) #in the left of the firstbox
-            self.wm.draw_text("Elemento", 15, self.box_x-self.spacing, self.box_y+75+15, self.wm.PURPLE) #in the left of the second box
-            self.wm.draw_text("Por Posição", 15, self.box_x*3-self.spacing, self.box_y+15, self.wm.PURPLE) #in the left of the firstbox
-            self.wm.draw_text("Por Elemento", 15, self.box_x*3-self.spacing-10, self.box_y+75+15, self.wm.PURPLE) #in the left of the second box
+            self.wm.add_img("imgs/ls.png", 400, 70)     #title
+            self.wm.add_img("imgs/inserir.png", self.box_x+55, self.box_y-30)  #in the top of first column
+            self.wm.add_img("imgs/remover.png", self.box_x*2+85, self.box_y-30)  #in the top of second column
+            self.wm.add_img("imgs/buscar.png", self.box_x*3+115, self.box_y-30)  #in the top of third column
+            
+            self.wm.add_img("imgs/posicao.png", self.box_x-self.spacing, self.box_y+15)  #in the left of first line
+            self.wm.add_img("imgs/elemento.png", self.box_x-self.spacing, self.box_y+50+15)  #in the left of second line
+            
+        
             
             if self.error_add:
                 self.wm.draw_text("ERROR", 18, self.box_x+55, self.box_y-55, self.wm.RED)
             if self.error_remove:
-                self.wm.draw_text("ERROR", 18, self.box_x*3+55, self.box_y-55, self.wm.RED)
+                self.wm.draw_text("ERROR", 18, self.box_x*2+85, self.box_y-55, self.wm.RED)
             if self.error_search:
-                self.wm.draw_text("ERROR", 18, self.box_x*4+55, self.box_y-55, self.wm.RED)
+                self.wm.draw_text("ERROR", 18, self.box_x*3+115, self.box_y-55, self.wm.RED)
 
-            self.wm.add_img("imgs/confirm.png", self.box_x+10, self.box_y+self.spacing+90)
-            self.wm.add_img("imgs/confirm.png", self.box_x*3+10, self.box_y+self.spacing+90)
-            self.wm.add_img("imgs/confirm.png", self.box_x*4+50, self.box_y+self.spacing+90)
+            self.wm.add_img("imgs/enviar.png", self.box_x+55, self.box_y+self.spacing+40)
+            self.wm.add_img("imgs/enviar.png", self.box_x*2+85, self.box_y+self.spacing+40)
+            self.wm.add_img("imgs/enviar.png", self.box_x*3+115, self.box_y+self.spacing+40)
 
             self.input_box1.draw(self.wm.display)
             self.input_box2.draw(self.wm.display)
@@ -88,7 +90,7 @@ class SequentialList():
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_position = pygame.mouse.get_pos() 
-                if self.wm.collide_point("imgs/confirm.png",self.box_x+10, self.box_y+self.spacing+90, mouse_position):
+                if self.wm.collide_point("imgs/enviar.png",self.box_x+55, self.box_y+self.spacing+40, mouse_position):
                     if self.input_box1.text != '' and self.input_box2.text != '':
                         if self.list.insert(int(self.input_box1.text), int(self.input_box2.text)) == False:
                             self.error_add = True
@@ -97,14 +99,14 @@ class SequentialList():
                     elif self.input_box1.text == '' or self.input_box2.text == '':
                         self.error_add = True
                 
-                elif self.wm.collide_point("imgs/confirm.png",self.box_x*3+10, self.box_y+self.spacing+90, mouse_position):
+                elif self.wm.collide_point("imgs/enviar.png",self.box_x*2+85, self.box_y+self.spacing+40, mouse_position):
                     if self.input_box3.text != '':
                         if self.list.remove(int(self.input_box3.text)) == None:
                             self.error_remove = True
                         else:
                             self.error_remove = False
                             
-                elif self.wm.collide_point("imgs/confirm.png",self.box_x*4+10, self.box_y+self.spacing+90, mouse_position):
+                elif self.wm.collide_point("imgs/enviar.png",self.box_x*3+115, self.box_y+self.spacing+40, mouse_position):
                     found = ''
                     if self.input_box4.text != '' and self.input_box5.text != '':
                         self.error_search = True
