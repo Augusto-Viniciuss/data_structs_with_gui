05b7fa7e9ce48dbd1b392df117a9ade833a01f62
Augusto Vinicius <augustoviniciud@gmail.com>
2023-05-15 15:27:44 -0300

    Done Binary Search Tree struct

---
+++ GUI/utils/Binary_search_tree.py
@@ -0,0 +1,60 @@
+from .Node import Node
+
+class Binary_search_tree:
+    def __init__(self):
+        self.root = None
+        self.empty = True
+
+    def is_empty(self):
+        return self.empty
+
+    def search_element(self, value):
+        if self.is_empty():
+            return False
+        else:
+            aux_node = self.root
+
+            while aux_node != None:
+                if aux_node.get_value() > value:
+                    aux_node = get_previous_node()
+                elif aux_node.get_value() < value:
+                    aux_node = get_next_node()
+                elif aux_node.get_value() == value:
+                    return True
+
+            return False
+
+    def insert(self, value):
+        if self.is_empty:
+            self.root = Node(value)
+            self.empty = False
+        else:
+            aux_node = self.root
+            
+            while True:
+                if value < aux_node.get_value():
+                    if aux_node.get_previous_node() == None:
+                        aux_node.set_previous_node = Node(value, aux_node, None)
+                        break
+                    else:
+                        aux_node = aux_node.get_previous_node()
+                elif value > aux_node.get_value():
+                    if aux_node.get_next_node() == None:
+                        aux_node.set_next_node = Node(value, None, aux_node)
+                        break
+                    else:
+                        aux_node = aux_node.get_next_node()
+
+        return True
+
+    def show_tree(self, node = self.root):
+        if self.is_empty():
+            return
+        else:
+            if node.get_previous_node() != None:
+                self.show_tree(node.get_previous_node())
+
+            print(node.get_value())
+
+            if node.get_next_node() != None:
+                self.show_tree(node.get_next_node())
